26046f71604c55dcdd30be5d2c68c43193ab7f78
rucen <ucen.rich@gmail.com>
2026-05-01 18:19:07 +0200

    Add Ad entity

---
+++ src/SimilaritySearch.Domain/Entities/Ad.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Pgvector;
+
+namespace SimilaritySearch.Domain.Entities;
+
+[Table(nameof(Ad))]
+public class Ad : Entity<Guid>
+{
+    public required string UserId { get; set; }
+    public required string UserName { get; set; }
+    public required string PhoneNumber { get; set; }
+    public string? Email { get; set; }
+    public required string Location { get; set; }
+    public required decimal Price { get; set; }
+    public required string Currency { get; set; } = "Kč";
+    
+    public required string Description { get; set; }
+    
+    // EmbeddingGemma-300M embedding output size 768
+    [Column(TypeName = "vector(768)")]
+    public Vector? DescriptionEmbedding { get; set; }
+
+    public bool IsReupload { get; set; } = false;
+    public string? ReuploadReason { get; set; }
+    
+    public bool IsDeleted { get; set; } = false;
+}
